bc3286283ec340c351f2aa7b0d2c8d6861c04596
chayma98 <43567381+chayma98@users.noreply.github.com>
2019-01-09 22:02:51 +0100

    Add files via upload
    this is the first time i used code blocks

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/********
+first attempt
+author : chayma bs
+date : 09/01/2019
+**********/
+int main() {
+int i=0 ;
+int j;
+for (i=1 ; i<=10 , i++){
+    j=i*2 ;
+    printf ("la valeur de j est: %d/n",j);
+}
+return 0 ;
+}
