ad45ee5b752107516ca7ba9163d738329c47eb7c
chayma98 <43567381+chayma98@users.noreply.github.com>
2019-01-24 23:00:17 +0100

    Add files via upload

---
+++ somme des nombre entiers.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int n,int k){
+    int i
+    int s = 0
+    for (i=1 to i<=n , i++){
+        if (n % i == 0{
+            s= s+1 ;}
+        if (s == k){d = d + i}
+    printf("la somme des % premiers nombres premiers est %d",k,d)
+    }
+}
