0ee021802c72bac6763ab5fe5a53eee51782ed79
chayma98 <43567381+chayma98@users.noreply.github.com>
2019-01-24 23:25:25 +0100

    Add files via upload

---
+++ nombre amis.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int somme (x) {
+int i ;
+int s = 0 ;
+for (i=2 to i<=n , i++ ){
+    if (x % i == 0 ){ s = s+ i;}
+}
+return s ;
+}
+int amis (x,y){
+if somme(x) == somme(y) {
+    printf ("% et % sont deux nombres amis");
+}}
