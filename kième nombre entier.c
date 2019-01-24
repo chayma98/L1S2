d4dc93990cad6fd794d03d40b19badaa93336ee8
chayma98 <43567381+chayma98@users.noreply.github.com>
2019-01-24 22:33:23 +0100

    Add files via upload

---
+++ kième nombre entier.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int n,int k){
+    int i
+    int s = 0
+    for (i=1 to i<=n , i++){
+        if (n % i == 0{
+            s= s+1 ;}
+        if (s == k) { printf("le %d ième nombre entiers est %d",k,i}
+    }
+    return 0
+}
