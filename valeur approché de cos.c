993a4b6a66263010d7821619d09bc3a14e5ca7c0
chayma98 <43567381+chayma98@users.noreply.github.com>
2019-01-24 23:18:11 +0100

    Add files via upload

---
+++ valeur approché de cos.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int fact(int x){
+int f = 1 ;
+while (x> 1){
+    f= f * x ;
+    n-- ;
+}
+return f}
+
+int somme( int x,int e){
+int i= 1;
+int y;
+int s = 0;
+y = pow(x,2*i+1*pow(1,i)/fact(2i+1);
+while (abs(y) =< e {
+    s = s+y ;
+    i++ ;}
+    return (s) ;
+}
