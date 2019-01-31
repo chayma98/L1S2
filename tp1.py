c39a3f77d052685cff4b41788847e9f5e7b008a8
chayma98 <43567381+chayma98@users.noreply.github.com>
2019-01-31 18:46:57 +0100

    Update tp1.py
    u have a prob with the first 2 functions

--- tp1.py
+++ tp1.py
@@ -6,24 +6,24 @@ TabReels = TypeVar(Iterable[float])
 MatReels = TypeVar(Iterable[TabReels]) 
 MAXPRODUITS = 100
 def saisir_produit (no : int , tabPrixNote : MatReels):
-    x = 0
-    y = 0
-    print("donner le prix",x)
-    tabPrixNote[0][no]= x 
-    print("donner la note",y)
+    x = int(input("donner le prix:"))
+    print("vous aver fourni le prix",x)
+    tabPrixNote[0][no]= x
+    y = int(input("donner la note:"))
+    print("vous avez fourni la note",y)
     tabPrixNote[1][no]= y
 def saisir_product (nb : int , tabPrixNote : MatReels):
-    x = 0
-    print ("donner le nombre produit cherché",x)
+    x = int(input("donner le nombre du produit recherché:"))
+    print ("vous avez fourni le nombre produit cherché",x)
     saisir_produit (x,tabPrixNote)
 def afficheDetailProduit (nb : int , tabPrixNote : MatReels):
-     saisir_produit (nb)
+     saisir_produit (nb,tabPrixNote)
 def affiche_produits (nb : int , tabPrixNote : MatReels):
     afficheDetailProduit (nb)
 def maxTab (nb : int , tab : TabReels) :
     i =1
     x=0 
-    for i in nb:
+    for i in range (nb):
         if tab[i]< tab[i+1]:
             x = tab[i+1]
 
@@ -31,21 +31,21 @@ def maxTab (nb : int , tab : TabReels) :
 def minTab (nb : int , tab : TabReels):
     i= 1
     x= 0
-    for i in nb :
+    for i in range( nb) :
         if tab[i]< tab[i+1] :
             x = tab[i]
     return x 
 def afficheMoinsCher (nb : int , tabPrixNote : MatReels):
     i=1 
     x=0
-    for i in nb :
+    for i in range  (nb) :
         if tabPrixNote[0][i]> tabPrixNote[0][i+1]:
             x = tabPrixNote [0][i+1]
-            afficheDetailProduit (x)
+            afficheDetailProduit (x, tabPrixNote)
 def afficheMieuxNote (nb : int , tabPrixNote : MatReels):
     i=1
     x=0
-    for i in nb :
+    for i in range (nb) :
         if tabPrixNote[1][i]> tabPrixNote[1][i+1]:
             x = tabPrixNote[1][i]
     afficheDetailProduit(x)
@@ -53,24 +53,24 @@ def afficheMieuxNote (nb : int , tabPrixNote : MatReels):
 def remplirTabNorme (nb : int , tab : TabReels , tabNorme : TabReels):# tab norme c'est la sortie
     i= 1
     x = maxTab (nb,tab)
-    for i in nb :
+    for i in range (nb) :
            tabNorme[i] = tab[i]/x 
-def trouverCompromis (nb: int , tabPrixNote : MatReels ):
+def trouverCompromis (nb: int , tabPrixNote : MatReels,tabNormNote : TabReels,tabNormPrix : TabReels ):
     remplirTabNorme(nb,tabPrixNote[0] , tabNormNote )
     remplirTabNorme(nb, tabPrixNote[1] , tabNormPrix  )
     
 def tabMixte (nb : int, tabNormNote: TabReels ,tabNormPrix : TabReels , tabMixte : TabReels):
-    for i in nb :
+    for i in range (nb) :
         tabMixte[i]= (tabNormNote[i]*(1-tabNormPrix[i]*0,9))
         
 def affichegeInteret (nb : int , tabMixte : TabReels):
     print (maxTab(nb , tabMixte))
     
-def moyennei ( nb: int, tab : TabReels):
+def moyenne ( nb: int, tab : TabReels):
     i = 1
     s = 0
     m = 0
-    for i in nb :
+    for i in range (nb) :
          s = s+ tab[i]
          m = s/nb
     return m      
@@ -79,9 +79,9 @@ def testTP1():
     notes:TabReels = array([2,4,5,3,2,0,2,5])
     prixNotes:MatReels = array([prix, notes])
     nb:int = len(prix)
-    afficher_moins_chers(nb, prixNotes)
-    afficher_mieux_notes(nb, prixNotes)
-    trouver_compromis(nb, prixNotes)
+    afficheMoinsCher(nb, prixNotes)
+    afficheMieuxNote(nb, prixNotes)
+    trouverCompromis(nb, prixNotes)
     print("Moyenne des prix : " + str(moyenne(nb, prix)))
     print("Moyenne des notes : " + str(moyenne(nb, notes)))
 
