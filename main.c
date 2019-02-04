844260ecd7e390bff75a8312046dcf5a64d46001
chayma98 <43567381+chayma98@users.noreply.github.com>
2019-02-04 17:00:28 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-void permuter (int x,int y){
- int z =0;
- z= x;
- x= y ;
- y= z;
+void saisir (int maxi , int TPolynome) {
+int i=0 ;
+int x =0 ;
+int c= 0 ; int p=0 ;
+for (i=1 ; i<= maxi ; i++){
+    printf ("donner la puissance %d \n et le coefficient %d",p,c);
+    scanf ("%d\n %d",&p,&c);
+    TPolynome[p]= c;
 }
-void triselec(int t[1000],int n) {
-int i =0 ;
-int j=0 ;
-for (i=1 ; i<=n ; i++){;
-    for ( (j=(i+1)) ; i<= n ; j++){
-        if (t[j]< t[i]){ permuter(&t[i],&t[j]);}
-    }
+void affichage (int maxi , int TPolynome){
+int i = 0 ;
+for (i=1 ; i<= maxi ; i++){
+    printf (saisir(maxi,TPolynome[i]));
 }
+def calcul (int maxi , int TPolynome){
+int i = 0 ;
+for (i=1 ; i<= maxi ; i++){
+    printf ("donner une valeur %d",x)
+    scanf ("%d",& x);
+    s = s + (t[i]*pow(x,i));}
+  return s ;
+}
+
+int main()
+{
+int TPolynome[maxi];
+const int maxi = 50;
+
 }
