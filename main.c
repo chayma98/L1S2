00f4270edceedbfc3e8131fd34260e202bf5ec14
chayma98 <43567381+chayma98@users.noreply.github.com>
2019-02-25 16:51:48 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,49 +2,62 @@
 #include <stdlib.h>
 #include <math.h>
 void saisitab (int maxi, int TPolynome[]){
-int i =0 ; int c=0;
-for (i=1 ; i<= maxi ; i++) {
-    printf("donner le coefiicient de la puissance i",c);
+int i =0 ;
+int c =0;
+printf
+for (i=0 ; i<= maxi ; i++) {
+    printf("donner le coefiicient de la puissance %d ",i);
     scanf("%d",&c);
         TPolynome[i]= c ;
 }}
-void affichageTab (int maxi , int *TPolynome){
+void affichageTab (int maxi , int TPolynome[]){
     int i = 0 ;
-    for (i=1;i<=maxi ; i++){
-           printf("\n %d \n",TPolynome[i]);
+    for (i=0;i<=maxi ; i++){
+           printf("\n %d\n",TPolynome[i]);
 }}
-void calcul (int maxi , int TPolynome[]){
+int calcul (int maxi , int TPolynome[]){
 int i = 0 ;
+int c = 0 ;
 int x = 0;
-printf("donner a valeur ",x);
+printf("donner la valeur %d ",x);
 scanf("%d",&x);
 for (i=1 ; i<= maxi ; i++){
-    TPolynome[i]*pow(x,i);
-}}
-void derivee(int maxi , int *TPolynome){
+    c = TPolynome[i]*pow(x,i);
+}
+return c ;}
+void derivee(int maxi , int TPolynome[]){
 int i = 0 ;
 int j = 0;
 for (i=2; i<= maxi ; i++){
     for (j=1 ; j<= maxi ; j++){
-       TPolynome[j]= TPolynome[i] ;
+       (TPolynome[j]= TPolynome[i]) ;
     }
 }}
-void produit (int maxi){
-saisitab(int maxi , int TPolynome1[]);
-saisitab(int maxi , int TPoynome2[]);
+void produit (int maxi,int TPolynome1[], int TPolynome2[]){
 int i = 0;
-int TPolynome3 [maxi];
+int TPolynome3[maxi];
 for (i=1 ; i<= maxi ; i++){
   TPolynome3[i] = (TPolynome1[i]*TPolynome2[i]);
 }}
-void somme ( int maxi){
+void somme ( int maxi,int TPolynome1[], int TPolynome2[]){
 int i = 0;
-saisitab(int maxi , int TPolynome1[]);
-saisitab(int maxi , int TPoynome2[]);
+int TPolynome3[maxi] ;
 for (i=1 ; i<= maxi ;i++){
-     printf ("\n %d \n "(TPolynome1[i])+TPolynome2[j]);}}
+     TPolynome3[i] = (TPolynome1[i]+TPolynome2[i]) ;
+     printf ("\n %d \n ", TPolynome3[i]);}}
 int main(){
-const int maxi = 2 ;
-int TPolynome [maxi];
+ int maxi = 50 ;
+typedef int TPolynome [maxi];
+typedef int TPolynomeA[maxi];
+typedef int TPolynomeB[maxi];
+saisitab(maxi , TPolynomeA);
+saisitab(maxi , TPolynomeB);
+somme(maxi , TPolynomeA, TPolynomeB);
+affichageTab(maxi ,TPolynomeA);
+affichageTab(maxi , TPolynomeB);
+derivee(maxi ,TPolynomeA);
+derivee(maxi ,TPolynomeB);
+somme(maxi, TPolynomeA , TPolynomeB);
+produit(maxi, TPolynomeA , TPolynomeB);
 return 0;
 }
