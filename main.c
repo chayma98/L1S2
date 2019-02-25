67e36ec44478a3c8dc3e410ec3bc73593411f443
chayma98 <43567381+chayma98@users.noreply.github.com>
2019-02-25 07:55:25 +0100

    Add files via upload
    TP3 unfinished

--- main.c
+++ main.c
@@ -1,31 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-void saisir (int maxi , int TPolynome) {
-int i=0 ;
-int x =0 ;
-int c= 0 ; int p=0 ;
-for (i=1 ; i<= maxi ; i++){
-    printf ("donner la puissance %d \n et le coefficient %d",p,c);
-    scanf ("%d\n %d",&p,&c);
-    TPolynome[p]= c;
-}
-void affichage (int maxi , int TPolynome){
+#include <math.h>
+void saisitab (int maxi, int TPolynome[]){
+int i =0 ; int c=0;
+for (i=1 ; i<= maxi ; i++) {
+    printf("donner le coefiicient de la puissance i",c);
+    scanf("%d",&c);
+        TPolynome[i]= c ;
+}}
+void affichageTab (int maxi , int *TPolynome){
+    int i = 0 ;
+    for (i=1;i<=maxi ; i++){
+           printf("\n %d \n",TPolynome[i]);
+}}
+void calcul (int maxi , int TPolynome[]){
 int i = 0 ;
+int x = 0;
+printf("donner a valeur ",x);
+scanf("%d",&x);
 for (i=1 ; i<= maxi ; i++){
-    printf (saisir(maxi,TPolynome[i]));
-}
-def calcul (int maxi , int TPolynome){
+    TPolynome[i]*pow(x,i);
+}}
+void derivee(int maxi , int *TPolynome){
 int i = 0 ;
+int j = 0;
+for (i=2; i<= maxi ; i++){
+    for (j=1 ; j<= maxi ; j++){
+       TPolynome[j]= TPolynome[i] ;
+    }
+}}
+void produit (int maxi){
+saisitab(int maxi , int TPolynome1[]);
+saisitab(int maxi , int TPoynome2[]);
+int i = 0;
+int TPolynome3 [maxi];
 for (i=1 ; i<= maxi ; i++){
-    printf ("donner une valeur %d",x)
-    scanf ("%d",& x);
-    s = s + (t[i]*pow(x,i));}
-  return s ;
-}
-
-int main()
-{
-int TPolynome[maxi];
-const int maxi = 50;
-
+  TPolynome3[i] = (TPolynome1[i]*TPolynome2[i]);
+}}
+void somme ( int maxi){
+int i = 0;
+saisitab(int maxi , int TPolynome1[]);
+saisitab(int maxi , int TPoynome2[]);
+for (i=1 ; i<= maxi ;i++){
+     printf ("\n %d \n "(TPolynome1[i])+TPolynome2[j]);}}
+int main(){
+const int maxi = 2 ;
+int TPolynome [maxi];
+return 0;
 }
