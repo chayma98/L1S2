586561ca2df101f671c1469a9f44a1f998c64ad6
chayma98 <43567381+chayma98@users.noreply.github.com>
2019-03-03 20:35:08 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 void saisitab (int maxi, int TPolynome[]){
 int i =0 ;
 int c =0;
-printf
 for (i=0 ; i<= maxi ; i++) {
     printf("donner le coefiicient de la puissance %d ",i);
     scanf("%d",&c);
@@ -28,28 +27,28 @@ return c ;}
 void derivee(int maxi , int TPolynome[]){
 int i = 0 ;
 int j = 0;
-for (i=2; i<= maxi ; i++){
-    for (j=1 ; j<= maxi ; j++){
+for (i=1; i<= maxi ; i++){
+    for (j=0 ; j<= maxi ; j++){
        (TPolynome[j]= TPolynome[i]) ;
     }
 }}
 void produit (int maxi,int TPolynome1[], int TPolynome2[]){
 int i = 0;
 int TPolynome3[maxi];
-for (i=1 ; i<= maxi ; i++){
+for (i=0 ; i<= maxi ; i++){
   TPolynome3[i] = (TPolynome1[i]*TPolynome2[i]);
 }}
 void somme ( int maxi,int TPolynome1[], int TPolynome2[]){
 int i = 0;
 int TPolynome3[maxi] ;
-for (i=1 ; i<= maxi ;i++){
+for (i=0 ; i<= maxi ;i++){
      TPolynome3[i] = (TPolynome1[i]+TPolynome2[i]) ;
-     printf ("\n %d \n ", TPolynome3[i]);}}
+     printf ("\n %d", TPolynome3[i]);}}
 int main(){
- int maxi = 50 ;
-typedef int TPolynome [maxi];
-typedef int TPolynomeA[maxi];
-typedef int TPolynomeB[maxi];
+ int maxi = 3;
+int TPolynome [maxi];
+int TPolynomeA[maxi];
+int TPolynomeB[maxi];
 saisitab(maxi , TPolynomeA);
 saisitab(maxi , TPolynomeB);
 somme(maxi , TPolynomeA, TPolynomeB);
