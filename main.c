e5c68d6eef4d8617c573471de273b6947055abac
chayma98 <43567381+chayma98@users.noreply.github.com>
2019-03-04 16:54:16 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,54 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
-void saisie(int matrice[][]){
-    int nbLigne = 0 ;
-    int nbColonnes = 0 ;
-    int i = 0 ;
-    int j = 0 ;
-    int c = 0 ;
-printf("donner le nbre des ligne %d et le nbre de colonnes %d", nbLigne ,nbColonnes);
-scanf("%d %d",&nbLigne,&nbColonnes);
-// la premiere dimension correspond au lignes et la deuxieme au colonne
-for (i=1 ; i<= nbLigne ; i++){
-        for (j= 1 ; j<= nbColonnes; j++){
-    printf("Elément[%d][%d] : ",i,j);
-    scanf("%d",&matrice[i][j]);}
+#include <math.h>
+void saisitab (int maxi, int TPolynome[]){
+int i =0 ;
+int c =0;
+for (i=0 ; i<= maxi ; i++) {
+    printf("donner le coefiicient de la puissance %d ",i);
+    scanf("%d",&c);
+        TPolynome[i]= c ;
 }}
-void affichage (int nbLigne , int nbColonnes, int matrice[nbLigne][nbColonnes]){
-    int i,j= 0;
-    if (nbColonnes < 20 ){
-        for (i=1 ; i<= nbLigne ; i++){
-        for (j=1 ; j<= nbColonnes ; j++){
-            printf ("%7d",matrice [i][j]);
-            printf("\n");
-        }}
-    else { printf("idk");}}
+void affichageTab (int maxi , int TPolynome[]){
+    int i = 0 ;
+    for (i=0;i<=maxi ; i++){
+           printf("\n %d\n",TPolynome[i]);
+}}
+int calcul (int maxi , int TPolynome[]){
+int i = 0 ;
+int c = 0 ;
+int x = 0;
+printf("donner la valeur %d ",x);
+scanf("%d",&x);
+for (i=1 ; i<= maxi ; i++){
+    c = TPolynome[i]*pow(x,i);
 }
-void zéroo (int nbLigne , int matrice){
-int i,j = 0 ;
-for (i=1; i<= nbLigne; i++){
-        for (j=1 ;j<= i ; j++){
-    matrice[i][j]= 0 ;}}}
-void transposé(int matrice , int nbLigne, int nbColonnes){
-int i,j = 0 ;
-int B[50][50];
- /* Affectation de la matrice transposée à B */
- for (i=0; i<nbLigne; i++)
-     for (j=0; j<nbColonnes ; j++)
-          B[j][i]=matrice[i][j];
- /* Affectation de la matrice transposée à B */
- for (i=0; i<nbLigne; i++)
-     for (j=0; j<nbColonnes; j++)
-          B[j][i]=matrice[i][j];
-  /* Edition du résultat */
- /* Attention: maintenant le rôle de nbLigne et nbcolonnes est inversé. */
-  printf("Matrice résultat :\n");
- for (i=0; i<nbColonnes; i++){
-     for (j=0; j<nbLigne; j++)
-     printf("%7d", B[i][j]);
-     printf("\n");}
+return c ;}
+int derivee (int p,int TPolynomeA[], int TPolynomeB[])
+  {
+  int i= 0;
+
+    if (p==-1)
+      return -1;
+    for (i=p;i>=1;i--)
+      TPolynomeA[i-1]=i*TPolynomeB[i];
+    return p-1;
+  }
+void produit (int maxi,int TPolynome1[], int TPolynome2[]){
+int i,j = 0;
+int TPolynome3[maxi];
+for (i=1 ; i<= maxi ; i++){
+        for (j=1;j<=i;j++){
+  TPolynome3[i] = (TPolynome1[i]*TPolynome2[j]);
+}}}
+void somme ( int maxi,int TPolynome1[], int TPolynome2[]){
+int i = 0;
+int TPolynome3[maxi] ;
+for (i=1 ; i<= maxi ;i++){
+     TPolynome3[i] = (TPolynome1[i]+TPolynome2[i]) ;
+     printf ("\n %d \n ", TPolynome3[i]);}}
+int affichageMenu()
+{int choixMenu ;
+     printf("---Menu---\n\n");
+     printf("1.saisitab\n");
+     printf("2.somme\n");
+     printf("3.produit\n");
+     printf("4.derivee\n");
+     printf("3.affichageTab\n");
+     printf("\nVotre choix?\n\n");
+     scanf("%d", &choixMenu);
+     return choixMenu ;
+}
+int choixpol () {
+    int choixpol;
+printf("1.polynome1 \n");
+printf("2.polynome2 \n ");
+scanf("%d", &choixpol);
+     return choixpol ;
+}
 int main(){
-//int maxi = 50 ;
-int matrice[50][50];
+ int maximum = 3 ;
+ int TPolynome [maximum];
+int TPolynomeA[maximum];
+int TPolynomeB[maximum];
+int maxi = 0 ;
+printf ("donner le plus haut degré du polynome %d",maxi);
+scanf("%d",&maxi);
+saisitab( maxi,TPolynome[maxi]);
+    switch (affichageMenu()) /* pourquoi choixMenu ca n'esxiste que dans la fonction affichageMenu */
+    {
+    case 1:
+        printf("Vous avez choisis le saisitab!\n");
+        switch (choixpol()){
+            case 1 :
+        saisitab(maxi , TPolynomeA); break ;
+            case 2 : printf("\n"); saisitab(maxi , TPolynomeB); break ; }
+        break;
+    case 2:
+        printf("Vous avez choisis la somme!");somme(maxi , TPolynomeA, TPolynomeB);
+        break;
+    case 3:
+        printf("Vous avez choisis le produit!");
+        break;
+    case 4:
+        printf("Vous avez choisi le derivé!");derivee(maxi ,TPolynomeA,TPolynomeB);
+derivee(maxi ,TPolynomeA,TPolynomeB);
+    case 5:
+        printf ("vous avez choisi d'afficher le tab");switch (choixpol()){
+            case 1 :
+                affichageTab(maxi ,TPolynomeA);break ;
+            case 2 :  affichageTab(maxi , TPolynomeB); break ; }
+        break;
+    default:
+        printf("Vous ne ferez rien du tout!");
+        break;
+    }
+
+    system("PAUSE");
+
 return 0;
 }
